b9aa08f6aee73cf4353c18258bc6319edbeb8dd2
leixiaorui <1150992388@qq.com>
2019-01-13 12:58:22 +0800

    提交打包的方式

--- pas-core/src/test/java/common/SpringTestCase.java
+++ pas-core/src/test/java/common/SpringTestCase.java
@@ -1,11 +1,11 @@
-package com.msjf.finance.pas.common.test;
+package common;
 @org.junit.runner.RunWith(org.springframework.test.context.junit4.SpringJUnit4ClassRunner.class)
 @org.springframework.test.context.ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
 public abstract class SpringTestCase extends org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests {
     public SpringTestCase() { /* compiled code */ }
 
     @org.junit.BeforeClass
-    public static void beforeClass() throws java.lang.NoSuchMethodException, java.net.MalformedURLException, java.lang.reflect.InvocationTargetException, java.lang.IllegalAccessException { /* compiled code */ }
+    public static void beforeClass() throws NoSuchMethodException, java.net.MalformedURLException, java.lang.reflect.InvocationTargetException, IllegalAccessException { /* compiled code */ }
 
     @org.junit.AfterClass
     public static void afterClass() { /* compiled code */ }
